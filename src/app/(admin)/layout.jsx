237e2e42a877e4b49a103c5cf52e8b8e66cc4a3d
Balbino <caremobilityservicesllc@gmail.com>
2026-04-03 12:32:42 -0400

    Add fixed admin sidebar rescue button

--- src/app/(admin)/layout.jsx
+++ src/app/(admin)/layout.jsx
@@ -1,4 +1,5 @@
 import LeftSideBar from '@/components/layouts/LeftSideBar';
+import LeftSideBarToggle from '@/components/layouts/TopBar/components/LeftSideBarToggle';
 import React from 'react';
 import { Container } from 'react-bootstrap';
 const layout = ({
@@ -6,6 +7,9 @@ const layout = ({
 }) => {
   return <>
       <LeftSideBar />
+      <div className="d-print-none" style={{ position: 'fixed', left: 10, top: 10, zIndex: 2100 }}>
+        <LeftSideBarToggle />
+      </div>
       <div className="startbar-overlay d-print-none" />
       <div className="page-wrapper">
         <div className="page-content">
